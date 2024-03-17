45182447d4907c03a0ed5377544703bc52456908
Jaco <jacovancr@gmail.com>
2024-03-17 15:33:06 +0100

    Dynamically determine the domain

--- src/App.php
+++ src/App.php
@@ -24,10 +24,26 @@ class App {
         ini_set('session.gc_maxlifetime', $sessionDuration);
         
         # Ensure cookies are set on the top level domain, so that the auth service works for all sub-domain sites. They access the same cookies.
-        $domain = '.' . $_ENV['APP_TOP_LEVEL_DOMAIN'];
-        if($_ENV['APP_ENV'] === 'local') {
-            $domain = 'localhost';
+        // Function to get the top level domain from a given host
+        function getTopLevelDomain($host) {
+            $parts = explode('.', $host);
+            $numParts = count($parts);
+        
+            if ($numParts > 2) {
+                // Return the last two parts of the domain
+                return $parts[$numParts - 2] . '.' . $parts[$numParts - 1];
+            }
+            return $host;
         }
+        
+        // Determine the domain based on the current request
+        $domain = ($_ENV['APP_ENV'] === 'local') ? 'localhost' : getTopLevelDomain($_SERVER['HTTP_HOST']);
+        
+        // Prefix with a dot to include all subdomains
+        if ($domain !== 'localhost') {
+            $domain = '.' . $domain;
+        }
+        
         session_set_cookie_params([
             'domain' => $domain,
             'lifetime' => $sessionDuration,
