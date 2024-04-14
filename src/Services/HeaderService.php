2b4dfcc627a281eaaa545a85a42efd7d9fbdc42c
Jaco <jacovancr@gmail.com>
2024-04-14 14:37:07 +0200

    Debug - error should not auth

--- src/Services/HeaderService.php
+++ src/Services/HeaderService.php
@@ -5,7 +5,7 @@ namespace App\Services;
 class HeaderService {
     public function send($header, $replace = true, $http_response_code = null) {
         $http_response_code === null 
-            ? header($header, $replace) 
+            ? header($header, $replace, null) 
             : header($header, $replace, $http_response_code);
     }
 }
