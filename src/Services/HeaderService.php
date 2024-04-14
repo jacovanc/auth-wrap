9952593e7e9847ef2df6e8df0c0b3de5f5757e39
Jaco <jacovancr@gmail.com>
2024-04-14 14:32:41 +0200

    Fix strict types for headerservice

--- src/Services/HeaderService.php
+++ src/Services/HeaderService.php
@@ -4,6 +4,8 @@ namespace App\Services;
 
 class HeaderService {
     public function send($header, $replace = true, $http_response_code = null) {
-        header($header, $replace, $http_response_code);
+        $http_response_code === null 
+            ? header($header, $replace) 
+            : header($header, $replace, $http_response_code);
     }
 }
